eb24630be7570298d8dd64f459c0427ef283f83c
Marina <marina_il@abv.bg>
2020-05-13 04:01:55 +0300

    Fix code and add commands

--- Hotel-Project/Hotel-Project.cpp
+++ Hotel-Project/Hotel-Project.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include<conio.h>
-#include "Hotel-Project.h"
+#include <time.h>
 #include <vector>
 #include <istream>
 #pragma warning(disable : 4996)
 
 struct Reservation {
 	int room;
-	char* date_from;
-	char* date_to;
+	time_t date_from;
+	time_t date_to;
 	char* note;
 	int guests;
 };
@@ -18,39 +18,52 @@ struct Room {
 	int room;
 	int guests;
 };
-struct Reservations {
-	int room, guest;
-	std::string from_date, to_date, note;
-};
 
-void readReservation();
 std::vector<Room> readRooms();
-std::vector<Reservations> readReservations();
+std::vector<Reservation> readReservations();
 
 void main() {
 	std::vector<Room> rooms = readRooms();
-	//std::vector<Reservations> reservation = readReservation();
-}
-
-void readReservation()
-{
-	std::vector<Reservations> reservation = readReservations();
+	char command[50];
+	do {
+		std::cout << "Type a command" << std::endl;
+		std::cin >> command;
+		if (std::strcmp(command, "open") == 0) {
+			std::vector<Reservation> reservation = readReservations();
+		}
+		if (std::strcmp(command, "clear") == 0) {
+			system("cls");
+		}
+		if (std::strcmp(command, "help") == 0) {
+			std::cout << "Here are list of commands that you can use:" << std::endl << std::endl;
+			std::cout << "open - This will open default file for all reservations" << std::endl;
+			std::cout << "clear - This will clear the terminal" << std::endl;
+			std::cout << "help - This will show this screen" << std::endl;
+			std::cout << "exit - This will close the app" << std::endl;
+		}
+	} while (std::strcmp(command, "exit") > 0);
 }
 
 std::vector<Room> readRooms() {
 	std::vector<Room> rooms;
-	char*  line;
+	char line[150];
 	std::ifstream in(".\\rooms.txt");
-	// тъй като теста ми беше със стринг, сега ще го преработя с char*, затова на този етап дава грешки 
-	while (std::getline(in, line)) {
-		int nom, guests;
+	bool isFirstLine = true;
 
-		size_t lasttab = line.find_last_of('\t');
-
-		nom = atoi(line.substr(0, lasttab).c_str());
-		int len = line.length() - lasttab - 1;
-		guests = atoi(line.substr(lasttab + 1, len).c_str());
-
+	while (in.getline(line, sizeof(line))) {
+		if (isFirstLine == true) {
+			isFirstLine = false;
+			continue;
+		}
+		char* param;
+		param = strtok(line, "\t");
+		int countParams = 0, nom = 0, guests = 0;
+		while (param != NULL) {
+			if (countParams == 0) nom = atoi(param);
+			if (countParams == 1) guests = atoi(param);
+			param = strtok(NULL, "\t");
+			countParams++;
+		}
 
 		Room cur_room;
 		cur_room.room = nom;
@@ -62,63 +75,93 @@ std::vector<Room> readRooms() {
 	return rooms;
 }
 		
-std::vector<Reservations> readReservations(){
-	std::vector<Reservations> reservation;
-	char*  line;
+std::vector<Reservation> readReservations(){
+	std::vector<Reservation> reservation;
+	char line[150];
 	std::ifstream in(".\\reservations.txt");
+	bool isFirstLine = true;
 
-	while (std::getline(in, line)) {
-		int room, guest;
-		char* from_date, to_date, note;
-		//100	2020-05-06	2020-05-13	2 with kid	3
-		//012(3)  456789[10]123(4) 56789[20]1234(5) 6789[30]12345(6) 7    [38]
-		//4 14 25 36
-		size_t lasttab = line.find_last_of('\t'); // Poslednia tab
+	while (in.getline(line, sizeof (line))) {
+		if (isFirstLine == true) {
+			isFirstLine = false;
+			continue;
+		}
+		int room = 0, guest = 0;
+		time_t from_date;
+		time_t to_date;
+		char note[50];
+		char* param;
+		int countParams = 0;
+		param = strtok(line, "\t"); 
+		while(param != NULL) {
+			if (countParams == 0) room = atoi(param);
+			if (countParams == 1) {
+				struct tm tmdate = { 0 };
+				tmdate.tm_year = atoi(&param[0]) - 1900;
+				tmdate.tm_mon = atoi(&param[5]) - 1;
+				tmdate.tm_mday = atoi(&param[8]);
+				from_date = mktime(&tmdate);
+			}
+			//if (countParams == 1) strcpy(from_date, param);
+			if (countParams == 2) {
+				struct tm tmdate = { 0 };
+				tmdate.tm_year = atoi(&param[0]) - 1900;
+				tmdate.tm_mon = atoi(&param[5]) - 1;
+				tmdate.tm_mday = atoi(&param[8]);
+				to_date = mktime(&tmdate);
+			}
+			//if (countParams == 2) strcpy(to_date, param);
+			if (countParams == 3) strcpy(note, param);
+			if (countParams == 4) guest = atoi(param);
 
-		size_t beforecomment = line.find_last_of('\t', lasttab - 1);
-		size_t beforedateto = line.find_last_of('\t', beforecomment - 1);
-		size_t beforedatefrom = line.find_last_of('\t', beforedateto - 1);
+			param = strtok(NULL, "\t");
+			countParams++;
+		}
 
-		room = atoi(line.substr(0, beforedatefrom).c_str());
-		int len = beforedateto - beforedatefrom - 1;
-		from_date = line.substr(beforedatefrom + 1, len).c_str();
-		len = beforecomment - beforedateto - 1;
-		to_date = line.substr(beforedateto + 1, len).c_str();
-		len = lasttab - beforecomment - 1;
-		note = line.substr(beforecomment + 1, len).c_str();
-		len = line.length() - lasttab - 1;
-		guest = atoi(line.substr(lasttab + 1, len).c_str());
-
-		//std::cout << room << from_date << to_date << note << guest << std::endl;
+		std::cout << room << from_date << to_date << note << guest << std::endl;
 		
 		
-		Reservations cur_reserve;
+		Reservation cur_reserve;
 		cur_reserve.room = room;
-		cur_reserve.guest = guest;
-		cur_reserve.from_date = from_date;
-		cur_reserve.to_date = to_date;
+		cur_reserve.guests = guest;
+		cur_reserve.date_from = from_date;
+		cur_reserve.date_to = to_date;
 		cur_reserve.note = note;
 
 		reservation.push_back(cur_reserve);
 	}
 	in.close();
 	return reservation;
-	
 }
 
-
-void main() {
-	std::ifstream infile("reservations.txt", std::ifstream::binary);
-	std::ofstream outfile("newReservations.txt", std::ofstream::binary);
-	infile.seekg(0, infile.end);
-	long size = infile.tellg();
-	infile.seekg(0);
-	char* buffer = new char[size];
-	infile.read(buffer, size);
-	outfile.write(buffer, size)
-	delete[] buffer;
-
-	outfile.close();
-	infile.close();
-	return;
-}
+//
+//void main() {
+//	std::ifstream infile(".\\reservations.txt", std::ifstream::binary);
+//	std::ofstream outfile(".\\newReservations.txt", std::ofstream::binary);
+//	infile.seekg(0, infile.end);
+//	long size = infile.tellg();
+//	infile.seekg(0);
+//	char* buffer = new char[size];
+//	infile.read(buffer, size);
+//	outfile.write(buffer, size)
+//	delete[] buffer;
+//
+//	outfile.close();
+//	infile.close();
+//	return;
+//
+//
+//
+//	FILE* pFile;
+//	pFile = fopen(".\\reservations.txt", "r");
+//	if (pFile == NULL)
+//	{
+//		printf("Error opening file");
+//		exit(EXIT_FAILURE);
+//	}
+//	else
+//	{
+//		/* file operations here */
+//	}
+//	return ;
+//}
